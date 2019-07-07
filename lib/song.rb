cf7853cfff4318b64cf6e6fe8b34019118c89dbe
Bernadette Davis <bsd231@nyu.edu>
2019-07-07 18:23:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,15 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize(name, artist)
+  end
+  
+  def self.create(name, artist)
+    self.initialize(name, artist)
+    @@all << self
+    self
+  end
+
   def self.all
     @@all
   end
