02010a61786e7b2d0cfab73244bcbcd955f1e098
Bernadette Davis <bsd231@nyu.edu>
2019-07-07 23:29:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,10 +40,10 @@ class Song
     end
 
     def self.find_or_create_by_name(input_name)
-      if self.find_by_name(input_name) == false
+      if (a = self.find_by_name(input_name)) == false
         self.create_by_name(input_name)
       else
-        self.find_by_name(input_name)
+        return a
       end
     end
 
